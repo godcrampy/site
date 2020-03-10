490688421379724050767a75f04412fb42338452
Sahil <sahilbondre@gmail.com>
2020-03-10 09:54:09 +0530

    setup sass, bulma, react-router, prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.scss";
+import HomePage from "./pages/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return <div className="HomePage">Home</div>;
+  }
+}
+
+export default HomePage;
