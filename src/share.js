bcb2ac7aca21aeac202b7fade7729687afaadbdd
Sahil Bondre <sahilbondre@gmail.com>
2020-12-18 12:18:20 +0530

    add links for hackathon masterclass

--- src/share.js
+++ src/share.js
@@ -7,5 +7,33 @@ export default {
       { text: "github", link: "https://github.com" },
       { text: "stackoverflow", link: "https://stackoverflow.com" }
     ]
+  },
+  masterclass: {
+    title: "The Hackathon Masterclass",
+    subtitle: "DSC NIT Surat + ACM Collab",
+    links: [
+      {
+        text: "Slides",
+        link:
+          "https://firebasestorage.googleapis.com/v0/b/mailing-list-16e98.appspot.com/o/The%20Hackathon%20Masterclass.pdf?alt=media&token=518d933d-72e1-472b-b3ce-a0e4c2bee588"
+      },
+      { text: "Aemie's Github", link: "https://github.com/aemieJ" },
+      { text: "Sahil's Github", link: "https://github.com/godcrampy" },
+      {
+        text: "Notes Kitty Webstore",
+        link:
+          "https://chrome.google.com/webstore/detail/notes-kitty/gmgfogccgeeimapbcgilfipbfenafbkp?hl=en"
+      },
+      { text: "Notes Kitty Repository", link: "https://github.com/Twelfth-Hour/notes-kitty" },
+      {
+        text: "Live Share",
+        link: "https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare-pack"
+      },
+      {
+        text: "Example Kanban on Github Projects",
+        link: "https://github.com/Twelfth-Hour/unicorn-savings/projects/1"
+      },
+      { text: "Dotslash", link: "https://www.hackdotslash.co.in" }
+    ]
   }
 };
